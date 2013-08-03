dda7764122aa3df1b37f69e62d42ebbeb25beed6
Amy Erz <wingmerz@gmail.com>
2013-08-03 12:14:00 -0700

    memelioniare

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memelioniare::Application.config.secret_token = '7ad13b8c604655f79074641cec5d246db96b7a5e20d6f2c64d4c2d5a1de7f30e44bcdb4235c3212505acbade09b970849050d0a28ad0709e7a703f54e0bbeba2'
